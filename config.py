12deb5976ff6c6b29d9ca11075f05690e3894d49
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-06-23 22:51:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7364613365:AAF7Jk49tj3YNe0PiYRXTHt1EyH8iENxVyM")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7273191470:AAGA7vvZ3MiUNVQH2QGxMZDvJD9J2mWQ9II")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "29759992"))
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "29759992"))
 API_HASH = os.environ.get("API_HASH", "61f150cdca64b2916fa499d107393140")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170820086"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002180653997"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5787502520"))
